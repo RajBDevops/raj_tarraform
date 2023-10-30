c4e9191d3731db8982ceae8f959138cbd92a08b3
Ubuntu <ubuntu@ip-172-31-31-119.us-east-2.compute.internal>
2023-10-30 11:10:35 +0000

    created pvt subnet A and B

--- vpc.tf
+++ vpc.tf
@@ -31,3 +31,25 @@ resource "aws_subnet" "tf-ecomm-pub-sn-B" {
   }
 }
 
+# Create pvt subnet -AZ- A
+resource "aws_subnet" "tf-ecomm-pvt-sn-A" {
+  vpc_id     = aws_vpc.tf-ecomm.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = "false"
+  tags = {
+    Name = "ecomm-pvt-subnet-A"
+  }
+}
+
+# Create pvt subnet -AZ- B
+resource "aws_subnet" "tf-ecomm-pvt-sn-B" {
+  vpc_id     = aws_vpc.tf-ecomm.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-2b"
+  map_public_ip_on_launch = "false"
+  tags = {
+    Name = "ecomm-pvt-subnet-B"
+  }
+}
+
