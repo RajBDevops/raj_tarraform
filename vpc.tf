1c7bc7b2ff4afe007ee2386cc5ae26523a759dad
Ubuntu <ubuntu@ip-172-31-31-119.us-east-2.compute.internal>
2023-10-30 10:38:35 +0000

    create pun subnet in zone A

--- vpc.tf
+++ vpc.tf
@@ -8,4 +8,13 @@ resource "aws_vpc" "tf-ecomm" {
   }
 }
 
+# Create public subnet -AZ- A
+resource "aws_subnet" "tf-ecomm-pub-sn-A" {
+  vpc_id     = aws_vpc.tf-ecomm.id
+  cidr_block = "10.0.0.0/24"
+availability_zone = "us-east-2"
+  tags = {
+    Name = "ecomm-public-subnet-A"
+  }
+}
 
