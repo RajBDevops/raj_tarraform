53476a99aa2f5aa4d7db81aab45bfc7ff5969689
Ubuntu <ubuntu@ip-172-31-31-119.us-east-2.compute.internal>
2023-10-30 10:55:32 +0000

    update public ip

--- vpc.tf
+++ vpc.tf
@@ -12,7 +12,8 @@ resource "aws_vpc" "tf-ecomm" {
 resource "aws_subnet" "tf-ecomm-pub-sn-A" {
   vpc_id     = aws_vpc.tf-ecomm.id
   cidr_block = "10.0.0.0/24"
-availability_zone = "us-east-2"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = "true"  
   tags = {
     Name = "ecomm-public-subnet-A"
   }
