70c793ff8801ad96739d75380bceee30b5cdd117
Ubuntu <ubuntu@ip-172-31-31-119.us-east-2.compute.internal>
2023-11-22 04:59:10 +0000

    security group

---
+++ ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "tf-ecomm-pub-sg" {
+  name        = "ecomm-web-sg"
+  description = "Allow ssh and http traffic"
+  vpc_id      = aws_vpc.tf-ecomm.id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+      }
+ ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+}
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  tags = {
+    Name = "ecomm-web-sg"
+  }
+}
